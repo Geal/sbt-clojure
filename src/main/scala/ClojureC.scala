92d9c7c77b212bebbfae698c99c9f7ddd7f8f38c
Geoffroy Couprie <geo.couprie@gmail.com>
2014-08-02 15:32:45 +0200

    compile all files and files in subdirectories

--- src/main/scala/ClojureC.scala
+++ src/main/scala/ClojureC.scala
@@ -10,7 +10,6 @@ class ClojureC(val classpath : Seq[File], val sourceDirectory : File, val stubDi
 
     lazy val oldContextClassLoader = Thread.currentThread.getContextClassLoader
 
-    //classpath.map(println)
     lazy val classLoader = ClasspathUtilities.toLoader(classpath)
     lazy val clojureClass = classLoader.loadClass("org.clojure.core")
     lazy val rt =  classLoader.loadClass("clojure.lang.RT")
@@ -21,54 +20,27 @@ class ClojureC(val classpath : Seq[File], val sourceDirectory : File, val stubDi
     lazy val rtInitFunction = rt.getDeclaredMethod("init")
     lazy val rtMap          = rt.getDeclaredMethod("map", classOf[Array[Object]])
 
-    /*lazy val projectClass = classLoader.loadClass("org.apache.tools.ant.Project")
-    lazy val generateStubsClass = classLoader.loadClass("org.codehaus.groovy.ant.GenerateStubsTask")
-    lazy val groovycClass = classLoader.loadClass("org.codehaus.groovy.ant.Groovyc")
-    lazy val javacClass = classLoader.loadClass("org.apache.tools.ant.taskdefs.Javac")
-    lazy val pathClass = classLoader.loadClass("org.apache.tools.ant.types.Path")
-
-    lazy val pathConstructor = pathClass.getConstructor(projectClass)
-    lazy val setLocationMethod = pathClass.getMethod("setLocation", classOf[java.io.File])
-
-    lazy val setGroovycSrcdirMethod = groovycClass.getMethod("setSrcdir", pathClass)
-    lazy val setGroovycStubdirMethod = groovycClass.getMethod("setStubdir", classOf[java.io.File])
-    lazy val setGroovycDestdirMethod = groovycClass.getMethod("setDestdir", classOf[java.io.File])
-    lazy val setGroovycProjectMethod = groovycClass.getMethod("setProject", projectClass)
-    lazy val addGroovycConfiguredJavacMethod = groovycClass.getMethod("addConfiguredJavac", javacClass)
-    lazy val setGroovycKeepStubsMethod = groovycClass.getMethod("setKeepStubs", java.lang.Boolean.TYPE)
-    lazy val setGroovycVerboseMethod = groovycClass.getMethod("setVerbose", java.lang.Boolean.TYPE)
-    lazy val executeGroovycMethod = groovycClass.getMethod("execute")*/
+    def getRecursiveListOfFiles(dir: File): Array[File] = {
+      val files = dir.listFiles
+      files ++ files.filter(_.isDirectory).flatMap(getRecursiveListOfFiles)
+    }
 
     def compile() : Unit =  {
         //println("clojurec compile source:      "+sourceDirectory)
         //println("clojurec compile stubs:       "+stubDirectory)
         //println("clojurec compile destination: "+destinationDirectory)
+        //getRecursiveListOfFiles(sourceDirectory).foreach(println)
+        val files = getRecursiveListOfFiles(sourceDirectory)
+        //files.filter(!_.isDirectory).map(println)
+
         IO.createDirectory(sourceDirectory)
         IO.createDirectory(destinationDirectory)
         try{
-          //Thread.currentThread.setContextClassLoader(classLoader)
-          /*val project = projectClass.newInstance()
-          val javac = javacClass.newInstance()
-          val groovyc = groovycClass.newInstance()
-          val path = pathConstructor.newInstance(project.asInstanceOf[AnyRef])
-          setLocationMethod.invoke(path, sourceDirectory)
-          setGroovycSrcdirMethod.invoke(groovyc, path.asInstanceOf[AnyRef])
-          setGroovycStubdirMethod.invoke(groovyc, stubDirectory)
-          setGroovycDestdirMethod.invoke(groovyc, destinationDirectory)
-          setGroovycProjectMethod.invoke(groovyc, project.asInstanceOf[AnyRef])
-          addGroovycConfiguredJavacMethod.invoke(groovyc, javac.asInstanceOf[AnyRef])
-          setGroovycKeepStubsMethod.invoke(groovyc, true.asInstanceOf[AnyRef])
-          setGroovycVerboseMethod.invoke(groovyc, true.asInstanceOf[AnyRef])
-          executeGroovycMethod.invoke(groovyc)*/
-
           Thread.currentThread().setContextClassLoader(classLoader)
-          //rtLoadFunction.invoke(null, Array("clojure/core"))
-          //loadResourceFunction.invoke(null, "src/main/clojure/hello.clj")
           rtInitFunction.invoke(null)
           val compilerClass   = classLoader.loadClass("clojure.lang.Compiler")
           val loadFunction    = compilerClass.getDeclaredMethod("load", classOf[java.io.Reader])
           val compileFunction = compilerClass.getDeclaredMethod("compile", classOf[java.io.Reader], classOf[java.lang.String], classOf[java.lang.String])
-          //loadFunction.invoke(null, new java.io.StringReader("(ns user) (println \"Hello from compiler\")"))
 
           val associativeClass = classLoader.loadClass("clojure.lang.Associative")
           val pushTBFunction = varClass.getDeclaredMethod("pushThreadBindings", associativeClass)
@@ -78,16 +50,16 @@ class ClojureC(val classpath : Seq[File], val sourceDirectory : File, val stubDi
           val compileFiles = varFunction.invoke(null, "clojure.core", "*compile-files*")
 
           val newMap = rtMap.invoke(null, Array(compilePath, destinationDirectory.getAbsolutePath(), compileFiles, true:java.lang.Boolean))
-          //val newMap = rtMap.invoke(null, Array(compilePath, "/Users/geal/dev/scala/clj/sbt-cljtest/pouet", compileFiles, true:java.lang.Boolean))
           pushTBFunction.invoke(null, newMap)
-          //println("sourcepath:      "+sourceDirectory.getAbsolutePath()+"/hello.clj")
-          //compileFunction.invoke(null, new java.io.StringReader("(ns user) (println \"Hello from compiler\")"), sourceDirectory.getAbsolutePath()+"/hello.clj", "coincoin.clj")
-          //compileFunction.invoke(null, new java.io.FileReader(sourceDirectory.getAbsolutePath()+"/hello.clj"), sourceDirectory.getAbsolutePath()+"/hello.clj", "hello.clj")
-          //compileFunction.invoke(null, new java.io.FileReader(sourceDirectory.getAbsolutePath()+"/hello.clj"), "src/main/clojure/hello.clj", "coincoin.clj")
-          compileFunction.invoke(null, new java.io.FileReader(sourceDirectory.getAbsolutePath()+"/hello.clj"), "hello.clj", "hello.clj")
+
+          files.filter(!_.isDirectory).map { f =>
+            //val suffix = f.toString.stripPrefix(sourceDirectory.toString)
+            compileFunction.invoke(null, new java.io.FileReader(f.getAbsolutePath()), f.getName(), f.getName())
+          }
+
           popTBFunction.invoke(null)
 
-          println("clojure compilation ended")
+          //println("clojure compilation ended")
         }
         finally{
           Thread.currentThread.setContextClassLoader(oldContextClassLoader)
